029521904ba2da1ea2eae7525d57001947a81164
Mendeleev <111556231+VbotR@users.noreply.github.com>
2023-11-15 00:02:24 +0300

    Update Monero.py

--- Monero.py
+++ Monero.py
@@ -1,26 +1,33 @@
-import os
-import subprocess
-import json
+# Импорт необходимых модулей
+import os  # Модуль для взаимодействия с операционной системой
+import subprocess  # Модуль для запуска внешних процессов
+import json  # Модуль для работы с JSON-данными
 
-wallet_address = input("Enter your Monero wallet address: ")
-print("Please choose a pool from the following options:")
+# Получение ввода от пользователя для адреса кошелька Monero
+wallet_address = input("Введите ваш адрес кошелька Monero: ")
+
+# Отображение доступных пулов для майнинга для выбора пользователя
+print("Пожалуйста, выберите пул из следующих вариантов:")
 print("1. pool.minexmr.com:4444")
 print("2. xmrpool.eu:9999")
 print("3. monerohash.com:5555")
 print("4. pool.supportxmr.com:5555")
-pool_choice = input("Enter the number of the pool you want to mine on: ")
-power_percent = input("Enter the percentage of mining power you want to use (0-100): ")
+pool_choice = input("Введите номер пула, на котором вы хотите майнить: ")
+power_percent = input("Введите процент майнинговой мощности, которую вы хотите использовать (0-100): ")
 
+# Преобразование power_percent в целое число и обработка неверного ввода
 try:
     power_percent = int(power_percent)
 except ValueError:
-    print("Invalid input for mining power. Please enter an integer between 0 and 100.")
+    print("Неверный ввод майнинговой мощности. Пожалуйста, введите целое число от 0 до 100.")
     exit()
 
+# Проверка допустимости введенного значения для майнинговой мощности
 if power_percent < 0 or power_percent > 100:
-    print("Invalid input for mining power. Please enter an integer between 0 and 100.")
+    print("Неверный ввод майнинговой мощности. Пожалуйста, введите целое число от 0 до 100.")
     exit()
 
+# Настройка адреса пула в соответствии с выбором пользователя
 if pool_choice == "1":
     pool_address = "pool.minexmr.com:4444"
 elif pool_choice == "2":
@@ -30,12 +37,13 @@ elif pool_choice == "3":
 elif pool_choice == "4":
     pool_address = "pool.supportxmr.com:5555"
 else:
-    print("Invalid input for pool choice. Please enter a number between 1 and 4.")
+    print("Неверный ввод для выбора пула. Пожалуйста, введите число от 1 до 4.")
     exit()
 
-xmrig_path = input("Enter the path to the XMRig executable file (e.g. C:/xmrig/xmrig.exe): ")
+# Получение пути к исполняемому файлу XMRig от пользователя
+xmrig_path = input("Введите путь к исполняемому файлу XMRig (например, C:/xmrig/xmrig.exe): ")
 
-# Configure XMRig automatically based on user inputs
+# Автоматическая настройка XMRig на основе введенных пользователем данных
 config_path = os.path.join(os.getcwd(), "config.json")
 
 config = {
@@ -67,19 +75,21 @@ config = {
     ]
 }
 
+# Запись настроек в файл конфигурации JSON
 with open(config_path, "w") as f:
     f.write(json.dumps(config))
 
+# Формирование команды для запуска XMRig
 command = f"{xmrig_path} -c {config_path}"
 
-# Run XMRig
+# Запуск XMRig в новом процессе
 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 
-# Open the pool website
+# Открытие веб-сайта пула в браузере
 pool_website = f"https://{pool_address.split(':')[0]}/"
 os.system(f"start {pool_website}")
 
-# Monitor output from XMRig
+# Мониторинг вывода из XMRig
 while True:
     output = process.stdout.readline().decode("utf-8").strip()
     if output == "":
